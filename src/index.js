f13bab2e8e03d6a85230208e7a3e4612dc38788c
zzinny <wwooww374@gmail.com>
2020-02-02 17:41:44 +0900

    개선6 : 무승부 표시하기

--- src/index.js
+++ src/index.js
@@ -121,6 +121,8 @@ class Game extends React.Component {
         if (winner) {
             status = 'Winner: ' + winner.winner;
             winnerArea = winner.winnerArea;
+        } else if (this.state.stepNumber === 9) {
+            status = 'Draw';
         } else {
             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
         }
